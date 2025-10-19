81783c8bb461695aae46e79793bb2c23e62a3919
leejaemin <jmlee4122@tukorea.ac.kr>
2025-10-19 16:49:05 +0900

    week-1.5 event_to_string.py 추가, 테스트: background 없데이트 (수업시간에 배운 내용 적용)

--- back_ground.py
+++ back_ground.py
@@ -1,8 +1,12 @@
 from pico2d import load_image
 
 from state_machine import StateMachine
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a
 
 file_path = '2DGP_background/stage_1/'
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
 
 class Stage01:
     def __init__(self, back_ground):
@@ -16,17 +20,14 @@ class Stage01:
         self.building_3 = load_image(file_path + 'bg_building_stage01_3.png')
         self.power_pole = load_image(file_path + 'bg_power_pole.png')
         self.fence = load_image(file_path + 'bg_fence.png')
-        pass
 
     def do(self):
         pass
 
-    def enter(self):
+    def enter(self, e):
         print("enter Stage01")
-        pass
 
-    def exit(self):
-        return Stage02(self.back_ground) # 다음 스테이지로 전환
+    def exit(self, e):
         pass
 
     def draw(self):
@@ -38,44 +39,65 @@ class Stage01:
         self.fence.draw(1920 / 2, 650)
         self.tile.draw(1920 / 2, 1080 / 2)
         self.power_pole.draw(1600, 1080 / 2)
-        pass
 
 class Stage02:
     def __init__(self, back_ground):
         self.back_ground = back_ground
 
         self.back_color = load_image(file_path + 'bg_color.png')
-        pass
 
     def do(self):
-        self.back_ground = Stage03(self.back_ground)
         pass
 
-    def enter(self):
+    def enter(self, e):
         print("enter Stage02")
-        pass
 
-    def exit(self):
+    def exit(self, e):
         pass
 
     def draw(self):
         self.back_color.draw(1920 / 2, 1080 / 2)
-        pass
 
 class Stage03:
     def __init__(self, back_ground):
+        self.back_ground = back_ground
+
+        self.back_color = load_image(file_path + 'bg_color.png')
+        self.cloud = load_image(file_path + 'bg_cloud.png')
+
+    def do(self):
         pass
+
+    def enter(self, e):
+        print("enter Stage03")
+
+    def exit(self, e):
+        pass
+
+    def draw(self):
+        self.back_color.draw(1920 / 2, 1080 / 2)
+        self.cloud.draw(1920 / 2, 1080 / 2)
 
 
 class BackGround:
     def __init__(self):
-        self.STAGE = Stage01(self)
-        self.STATE_MACHINE = StateMachine(self.STAGE)
-        pass
+        self.STAGE_01 = Stage01(self)
+        self.STAGE_02 = Stage02(self)
+        self.STAGE_03 = Stage03(self)
+        self.STATE_MACHINE = StateMachine(
+            self.STAGE_01,
+            {
+                self.STAGE_01: {space_down: self.STAGE_02},
+                self.STAGE_02: {space_down: self.STAGE_03},
+                self.STAGE_03: {space_down: self.STAGE_01}
+            }
+        )
 
     def update(self):
         self.STATE_MACHINE.update() # 상태 머신으로 하여금 업데이트
 
     def draw(self):
         self.STATE_MACHINE.draw() # 상태 머신으로 하여금 그리기
-        pass
+
+    def handle_event(self, event):
+        self.STATE_MACHINE.handle_state_event(('INPUT', event))
--- main.py
+++ main.py
@@ -14,46 +14,36 @@ def reset_world():
 
 def handle_events():
     global running
-    global back_ground_update
 
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
-            elif event.key == SDLK_SPACE: # 스페이스바를 누르면 배경이 바뀜 (테스트 용도)
-                back_ground_update = True
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            backGround.handle_event(event)
+
 
 def update_world():
-    global back_ground_update
     for o in world:
-        if o == backGround:
-            if back_ground_update:
-                o.update()
-                back_ground_update = False
-        else:
-            o.update()
-    pass
+        o.update()
 
 def render_world():
     clear_canvas()
     for o in world:
         o.draw()
     update_canvas()
-    pass
 
 running = True
-back_ground_update = False
 
 open_canvas(1920, 1080)
 reset_world()
 
 while running:
+    handle_events()
     update_world()
     render_world()
-    handle_events()
     delay(0.01)
 
 close_canvas()
