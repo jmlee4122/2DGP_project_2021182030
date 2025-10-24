f9ece3c05f25cc25745464087d734e31ebab985b
leejaemin <jmlee4122@tukorea.ac.kr>
2025-10-24 18:59:52 +0900

    week-2.3 run 상태 구현

--- user_character.py
+++ user_character.py
@@ -2,6 +2,15 @@ from pico2d import load_image, get_time
 from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a
 from state_machine import StateMachine
 
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
 class Death:
     def __init__(self, user_character):
         self.user_char = user_character
@@ -38,22 +47,46 @@ class Run:
     def __init__(self, user_character):
         self.uc = user_character
         file_path = '2DGP_character/user_character/'
-        self.image = load_image(file_path + 'user_idle_sprite_sheet.png')
+        self.image = load_image(file_path + 'user_running_sprite_sheet.png')
         self.clip_width = 402
         self.clip_height = 382
         self.clip_bottom = 0
+        self.frame = 0
 
     def enter(self, e):
-        pass
+        if right_down(e) or left_up(e):
+            self.uc.delta_move = self.uc.face_dir = 1
+            self.clip_bottom = 0
+        elif left_down(e) or right_up(e):
+            self.uc.delta_move = self.uc.face_dir = -1
+            self.clip_bottom = 2
 
     def exit(self, e):
         pass
 
     def do(self):
-        pass
+        self.uc.frame = (self.uc.frame + 1) % 12
+        self.frame = self.uc.frame
+        self.uc.x += self.uc.delta_move * 5
+
+        if self.uc.face_dir == 1 and self.uc.frame >= 6:
+            self.clip_bottom = 1
+            self.frame -= 6
+        elif self.uc.face_dir == -1 and self.uc.frame >= 6:
+            self.clip_bottom = 3
+            self.frame -= 6
 
     def draw(self):
-        pass
+        if self.uc.face_dir == 1:  # right
+            self.image.clip_draw(
+                self.frame * self.clip_width, self.clip_bottom * self.clip_height,
+                self.clip_width, self.clip_height, self.uc.x, self.uc.y, 300, 300
+            )
+        else:  # face_dir == -1: # left
+            self.image.clip_draw(
+                self.frame * self.clip_width, self.clip_bottom * self.clip_height,
+                self.clip_width, self.clip_height, self.uc.x, self.uc.y, 300, 300
+            )
 
 
 class Idle:
@@ -66,7 +99,7 @@ class Idle:
         self.clip_bottom = 0
 
     def enter(self, e):
-        pass
+        self.uc.delta_move = 0
 
     def exit(self, e):
         pass
@@ -77,11 +110,13 @@ class Idle:
     def draw(self):
         if self.uc.face_dir == 1:  # right
             self.image.clip_draw(
-                3 * self.clip_width, self.clip_bottom, self.clip_width, self.clip_height, self.uc.x, self.uc.y, 300, 300
+                3 * self.clip_width, self.clip_bottom * self.clip_height,
+                self.clip_width, self.clip_height, self.uc.x, self.uc.y, 300, 300
             )
         else:  # face_dir == -1: # left
             self.image.clip_draw(
-                0 * self.clip_width, self.clip_bottom, self.clip_width, self.clip_height, self.uc.x, self.uc.y, 300, 300
+                0 * self.clip_width, self.clip_bottom * self.clip_height,
+                self.clip_width, self.clip_height, self.uc.x, self.uc.y, 300, 300
             )
 
 class UserChar:
@@ -99,8 +134,8 @@ class UserChar:
         self.STATE_MACHINE = StateMachine(
             self.IDLE,  # 시작상태
             {  # 룰
-                self.IDLE: {},
-                self.RUN: {},
+                self.IDLE: {right_up: self.RUN, left_up: self.RUN, right_down: self.RUN, left_down: self.RUN},
+                self.RUN: {right_down: self.IDLE, left_down: self.IDLE, right_up: self.IDLE, left_up: self.IDLE},
                 self.JUMP: {},
                 self.DEATH: {}
             }
