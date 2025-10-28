64492d7a76e88f6cc476da0f4cfb98137f840fe1
leejaemin <jmlee4122@tukorea.ac.kr>
2025-10-28 19:11:17 +0900

    week-3.1 game_world.remove_object() 매개변수 수정

--- game_world.py
+++ game_world.py
@@ -3,7 +3,7 @@ world = [[], []]
 def add_object(o, depth):
     world[depth].append(o)
 
-def remove_object(o, depth):
+def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
