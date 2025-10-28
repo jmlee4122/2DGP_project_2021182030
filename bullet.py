a8bbe6c20cf6e369fa185d776efae78f36d70c9d
leejaemin <jmlee4122@tukorea.ac.kr>
2025-10-28 20:14:31 +0900

    week-3.5 총알이 사거리에 도달하면 그 총알 객체를 삭제, 변화 내용을 update()안에

--- bullet.py
+++ bullet.py
@@ -3,15 +3,19 @@ import game_world
 
 class Bullet:
     image = None
-    def __init__(self, x, y, face):
+    def __init__(self, x, y, velocity):
         if not Bullet.image:
             file_path = '2DGP_attack/'
             self.image = load_image(file_path + 'uc_bullet.png')
-        self.x, self.y, self.face = x, y, face
+        self.x, self.y, self.velocity = x, y, velocity
+        self.range, self.dis = 500, 0
 
     def draw(self):
         self.image.draw(self.x, self.y, 24, 14)
-        self.x = self.x + self.face
+
 
     def update(self):
-        pass
+        self.x = self.x + self.velocity
+        self.dis = self.dis + self.velocity
+        if self.dis > self.range or self.dis < -self.range:
+            game_world.remove_object(self)
