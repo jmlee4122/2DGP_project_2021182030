f56756ceb4f6f05480c470d7d2cc3c2839ee5e37
leejaemin <jmlee4122@tukorea.ac.kr>
2025-11-10 20:58:36 +0900

    week-5.10 user_char down_run 구현

--- user_character.py
+++ user_character.py
@@ -84,9 +84,15 @@ class Death:
 class DownRun:
     def __init__(self, user_character):
         self.uc = user_character
+        file_path = '2DGP_character/user_character/'
+        self.image = load_image(file_path + 'user_r_down.png')
 
     def enter(self, e):
-        pass
+        file_path = '2DGP_character/user_character/'
+        if self.uc.face_dir == 1:
+            self.image = load_image(file_path + 'user_r_down.png')
+        else:
+            self.image = load_image(file_path + 'user_l_down.png')
 
     def exit(self, e):
         pass
@@ -95,7 +101,7 @@ class DownRun:
         pass
 
     def draw(self):
-        pass
+        self.image.draw(self.uc.x, self.uc.y)
 
 class DownIdle:
     def __init__(self, user_character):
