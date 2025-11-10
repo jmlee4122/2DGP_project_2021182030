8d1cf205e7b8e1fae52e3a8c910fec9b915c5ef0
leejaemin <jmlee4122@tukorea.ac.kr>
2025-11-10 22:41:26 +0900

    week-5.14 basic monster 의 fire 가 일정 시간에 맞게 모션 생성

--- basic_monster.py
+++ basic_monster.py
@@ -20,7 +20,7 @@ TIME_PER_ACTION_DEATH = 0.3
 ACTION_PER_TIME_DEATH = 1.0 / TIME_PER_ACTION_DEATH
 FRAMES_PER_ACTION_DEATH = 8
 
-TIME_PER_ACTION_IDLE = 1.0
+TIME_PER_ACTION_IDLE = 3.0
 ACTION_PER_TIME_IDLE = 1.0 / TIME_PER_ACTION_IDLE
 FRAMES_PER_ACTION_IDLE = 5
 
@@ -79,7 +79,7 @@ class Idle:
     def enter(self, e):
         file_path = '2DGP_character/basic_monster/'
         self.basic.image = load_image(file_path + 'basic_idle_sprite_sheet.png')
-        self.basic.frame = 0
+        #self.basic.frame = 0
         self.basic.clip_size_x = 402
         self.basic.clip_size_y = 382
         self.basic.wait_time = get_time()
@@ -87,11 +87,10 @@ class Idle:
     def exit(self, e):
         if e and e[0] == 'TIMEOUT':
             self.basic.attack()
-            self.basic.is_attacking = True
 
     def do(self):
         self.basic.frame = (self.basic.frame + FRAMES_PER_ACTION_IDLE * ACTION_PER_TIME_IDLE * game_framework.frame_time) % 5
-        self.basic.frame = 0
+        #self.basic.frame = 0
         if self.basic.user.x < self.basic.x:
             self.basic.face_dir = -1
         else:
@@ -101,17 +100,11 @@ class Idle:
 
     def draw(self):
         if self.basic.face_dir == 1:
-            self.basic.image.clip_draw(0, 0 * self.basic.clip_size_y, self.basic.clip_size_x, self.basic.clip_size_y,
-                                       self.basic.x, self.basic.y)
+            self.basic.image.clip_draw(int(self.basic.frame) * self.basic.clip_size_x, 0 * self.basic.clip_size_y,
+                                       self.basic.clip_size_x, self.basic.clip_size_y, self.basic.x, self.basic.y)
         elif self.basic.face_dir == -1:
-            self.basic.image.clip_draw(0, 1 * self.basic.clip_size_y, self.basic.clip_size_x, self.basic.clip_size_y,
-                                       self.basic.x, self.basic.y)
-        # if self.basic.face_dir == 1:
-        #     self.basic.image.clip_draw(int(self.basic.frame) * self.basic.clip_size_x, 0 * self.basic.clip_size_y,
-        #                                self.basic.clip_size_x, self.basic.clip_size_y, self.basic.x, self.basic.y)
-        # elif self.basic.face_dir == -1:
-        #     self.basic.image.clip_draw(int(self.basic.frame) * self.basic.clip_size_x, 1 * self.basic.clip_size_y,
-        #                                self.basic.clip_size_x, self.basic.clip_size_y, self.basic.x, self.basic.y)
+            self.basic.image.clip_draw(int(self.basic.frame) * self.basic.clip_size_x, 1 * self.basic.clip_size_y,
+                                       self.basic.clip_size_x, self.basic.clip_size_y, self.basic.x, self.basic.y)
 
 
 class BasicMonster:
